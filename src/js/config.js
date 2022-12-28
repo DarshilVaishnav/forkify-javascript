f39de7d79d41f432149d0fe67eb70c4512f05b8a
Darshil Vaishnav <darshilvaishnav69@gmail.com>
2022-12-28 11:25:23 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '05022515-5a0c-4c21-b90a-875833ac194c';
+export const MODAL_CLOSE_SEC = 2.5;
